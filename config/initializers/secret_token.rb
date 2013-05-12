637d003dada70fb911886a02a67d66a9b5045c3b
yshimizu <yshimizu@tlab.(none)>
2013-05-12 22:38:09 +0900

    2.4.5 prepare data using fixtures.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = 'bec988bf3df9f0df955e7110f8dd2ae9bf94eefb7ab8fe060b227054be4aedb6b89457dd8c872620f7288e8b418f3a7c5e715211c0a95508bfa361a632290942'
